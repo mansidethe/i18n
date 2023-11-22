5a68b53464085de8a6fa4a66aaf2e167af3282e7
mansidethe <mansidethe18@gmail.com>
2023-11-23 00:54:24 +0530

    add design

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -6,14 +6,19 @@ function Home() {
   const usersCount = 15;
 
   return (
-    <div>
-        <h1>{i18n("welcomemessage")}</h1>
-        <p>{i18n("normalmessage")}</p>
-        <h2>{i18n("greetingmessage")}</h2>
-        <h5>{i18n("endMessage")}</h5>
-       
+    <div className='main-container'>
+     
+        <h1 className='text-center'>{i18n("welcomemessage")}</h1>
 
+        <div className='container'>
+        <h3>{i18n("normalmessage")}</h3>
+        <h2>{i18n("userstack", "<studentCount>",usersCount)}</h2>
+        <h2 className='text'>{i18n("greetingmessage")}</h2>
+        </div>
+       
+<div className='select-option'>
         <select 
+        
          defaultValue={localStorage.getItem("lang")}
         onChange={(e)=>{
           localStorage.setItem("lang", e.target.value);
@@ -23,8 +28,8 @@ function Home() {
         <option value='hi'>Hindi</option>
         <option value='en'>English</option>
         </select>
-
-        <h5>{i18n("userstack", "<studentCount>",usersCount)}</h5>
+        </div>
+        
     </div>
   )
 }
