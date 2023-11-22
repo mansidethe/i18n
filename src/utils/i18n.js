e347aa1f2a2292885de700785bb751b6c0b05e58
mansidethe <mansidethe18@gmail.com>
2023-11-23 00:04:40 +0530

    added json file and i18n file

---
+++ src/utils/i18n.js
@@ -0,0 +1,15 @@
+import languageConfig from './lang.json';
+
+const i18n = (keyword, placeholder, value)=>{
+    const lang = localStorage.getItem("lang") || "en";
+
+    const text = languageConfig[lang][keyword];
+
+    if(placeholder && value){
+        return text.replace(placeholder, value);
+    }
+
+    return text;
+}
+
+export default i18n;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Home.css'
+import i18n from './../../utils/i18n'
 
 function Home() {
+  const usersCount = 15;
+
   return (
-    <div>Home</div>
+    <div>
+        <h1>{i18n("welcomemessage")}</h1>
+        <p>{i18n("normalmessage")}</p>
+        <h2>{i18n("greetingmessage")}</h2>
+        <h5>{i18n("endMessage")}</h5>
+       
+
+        <select 
+         defaultValue={localStorage.getItem("lang")}
+        onChange={(e)=>{
+          localStorage.setItem("lang", e.target.value);
+          window.location.reload();
+        }}>
+        <option value='mr'>Marathi</option>
+        <option value='hi'>Hindi</option>
+        <option value='en'>English</option>
+        </select>
+
+        <h5>{i18n("userstack", "<studentCount>",usersCount)}</h5>
+    </div>
   )
 }
 
